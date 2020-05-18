2003abbe7d558fb29eaa35572996f11b9184f7ef
Millsionaire <Michael.MillsATX@gmail.com>
2020-05-17 23:59:28 -0500

    Divider Lines
    Added divider lines to terms list

--- app/src/main/java/com/wgu/c196/TermsActivity.java
+++ app/src/main/java/com/wgu/c196/TermsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -72,6 +73,9 @@ public class TermsActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration divider = new DividerItemDecoration(mRecyclerView.getContext(), layoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(divider);
     }
 
     @Override
